8f5f43d3bd3ec6b6cdbdea75a0d05702acf17ae1
Jeevapriya14 <jeevapriya1473@gmail.com>
2025-06-18 01:01:23 +0530

    added commandpattern

---
+++ DesignPatternsAndPrinciple/CommandPatternExample/LightCommandOff.java
@@ -0,0 +1,12 @@
+public class LightCommandOff implements Command {
+    private Light light;
+
+    public LightOffCommand(Light light) {
+        this.light = light;
+    }
+
+    @Override
+    public void execute() {
+        light.turnOff();
+    }
+}
