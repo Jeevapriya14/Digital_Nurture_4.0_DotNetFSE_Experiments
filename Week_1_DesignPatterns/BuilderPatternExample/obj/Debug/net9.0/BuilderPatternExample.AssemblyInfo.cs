5cf9ccbcee8ec89b191fab16faa1b9e7b0e00a06
Jeevapriya14 <jeevapriya1473@gmail.com>
2025-06-21 15:53:12 +0530

    added week 1 datastrcutures

--- Week_1_DesignPatterns/BuilderPatternExample/obj/Debug/net9.0/BuilderPatternExample.AssemblyInfo.cs
+++ Week_1_DesignPatterns/BuilderPatternExample/obj/Debug/net9.0/BuilderPatternExample.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BuilderPatternExample")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03755634eb7aef605325ce8738faf6e397dc4e13")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cbecf9f34a7d7d0e8a15e9297c93d8e34af69a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("BuilderPatternExample")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BuilderPatternExample")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
