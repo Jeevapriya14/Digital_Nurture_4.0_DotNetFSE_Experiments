d11a2e58b6483a5908355e18babf93b2e8d6b548
Jeevapriya14 <jeevapriya1473@gmail.com>
2025-06-21 20:08:49 +0530

    added financial forecasting

--- Week_1_DataStructures/E-commerce_Platform_Search_Option/obj/Debug/net9.0/E-commerce_Platform_Search_Option.AssemblyInfo.cs
+++ Week_1_DataStructures/E-commerce_Platform_Search_Option/obj/Debug/net9.0/E-commerce_Platform_Search_Option.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E-commerce_Platform_Search_Option")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41974ff1fc39dc4b34e51d9628bf381fa4e68b70")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30d1238a2d7573374762cae16091b5da6c96ccd3")]
 [assembly: System.Reflection.AssemblyProductAttribute("E-commerce_Platform_Search_Option")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E-commerce_Platform_Search_Option")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Week_1_DataStructures/Financial_Forecasting/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static double PredictFutureValue(double presentValue, double rate, int years)
+    {
+        if (years == 0)
+            return presentValue;
+
+        return (1 + rate) * PredictFutureValue(presentValue, rate, years - 1);
+    }
+
+    static void Main()
+    {
+        double presentValue = 10000; 
+        double annualGrowthRate = 0.08; 
+        int years = 5;
+
+        double futureValue = PredictFutureValue(presentValue, annualGrowthRate, years);
+
+        Console.WriteLine($"Predicted Future Value after {years} years: ₹{futureValue:F2}");
+    }
+}
