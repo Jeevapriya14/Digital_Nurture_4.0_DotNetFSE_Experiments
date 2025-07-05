81b053b487f20b56ffe8e3b1938a70fe228782ff
Jeevapriya14 <jeevapriya1473@gmail.com>
2025-07-05 15:03:29 +0530

    added lab01

---
+++ Week_3_Entity_Framework/RetailInventory/Migrations/20250705091916_SeedDataAdded.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace RetailInventory.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedDataAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Electronics" },
+                    { 2, "Groceries" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "CategoryId", "Name", "Price" },
+                values: new object[,]
+                {
+                    { 1, 1, "Smartphone", 25000m },
+                    { 2, 2, "Wheat Flour", 800m }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
---
+++ Week_3_Entity_Framework/RetailInventory/Program.cs
@@ -0,0 +1,17 @@
+﻿using RetailInventory.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace RetailInventory;
+
+class Program {
+    static void Main() {
+        using var context = new AppDbContext();
+
+        context.Database.Migrate();
+
+        var products = context.Products.Include(p => p.Category).ToList();
+        foreach (var p in products) {
+            Console.WriteLine($"{p.Name} - ₹{p.Price} - {p.Category?.Name}");
+        }
+    }
+}
