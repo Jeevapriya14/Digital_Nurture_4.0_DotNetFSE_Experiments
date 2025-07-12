f0e08d6441407562814e7989646f445cd50cbe83
Jeevapriya14 <jeevapriya1473@gmail.com>
2025-07-13 01:27:34 +0530

    Added week 04 lab

--- Week2_SQL,NUNIT,MOQ/Mandatory-Exercises/Exercise4-NUNIT/CalcLibrary/obj/Debug/netstandard2.0/CalcLibrary.AssemblyInfo.cs
+++ Week2_SQL,NUNIT,MOQ/Mandatory-Exercises/Exercise4-NUNIT/CalcLibrary/obj/Debug/netstandard2.0/CalcLibrary.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalcLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2535d0652b1da3103588e2e532bd1e60d65a3d3e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b3137e019ae645d3ab1530658c989da7478c813")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalcLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalcLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
